bd62ad5250a1d756b7283a4506d042b3bd3c6664
Danillo Santos <dnashy@hotmail.com>
2023-11-02 12:54:01 -0300

    feat: add the dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html',
+    './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#373F51",
+        "primary-300": "#292F3D",
+        "primary-500": "#181C25",
+        "secondary-400": "#D8DBE2",
+        "secondary-500": "#C4C9D4",
+        "highlight-100": "#2892D7",
+        "highlight-300": "#2380BE",
+      },
+      backgroundImage: () => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+      height: {
+        200: "1000px",
+        110: "440px",
+      },
+      width: {
+        112: "600px",
+        90: "86vw",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
+
